9756ef06b0e70771b7cfa4cf71d39890b9207c2b
IlijaRad <ilija.radivojevic237@gmail.com>
2025-06-20 16:57:01 +0200

    WIP

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -27,7 +26,9 @@ class JobController extends Controller
     public function store(){
         request()->validate(['title' => ['required', 'min:3'], 'salary' => ['required']]);
 
-        Job::create(['title' => request('title'), 'salary' => request('salary'), 'employer_id' => 1]);
+        $job = Job::create(['title' => request('title'), 'salary' => request('salary'), 'employer_id' => 1]);
+
+        Mail::to($job->employer->user)->queue(new JobPosted($job));
 
         return redirect('/jobs');
     }
